d41e3c7d5d325aed672f8998a5f9bf1bae36ac5b
Senior Fall <zizasenior@gmail.com>
2023-04-26 21:57:26 -0400

    Update register and logout

--- TestAuthentification/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TestAuthentification/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -25,18 +25,18 @@ namespace TestAuthentification.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly IUserStore<IdentityUser> _userStore;
-        private readonly IUserEmailStore<IdentityUser> _emailStore;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserStore<ApplicationUser> _userStore;
+        private readonly IUserEmailStore<ApplicationUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
         private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
-            UserManager<IdentityUser> userManager,
-            IUserStore<IdentityUser> userStore,
-            SignInManager<IdentityUser> signInManager,
+            UserManager<ApplicationUser> userManager,
+            IUserStore<ApplicationUser> userStore,
+            SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender,
             RoleManager<IdentityRole> roleManager)
@@ -187,13 +187,13 @@ namespace TestAuthentification.Areas.Identity.Pages.Account
             }
         }
 
-        private IUserEmailStore<IdentityUser> GetEmailStore()
+        private IUserEmailStore<ApplicationUser> GetEmailStore()
         {
             if (!_userManager.SupportsUserEmail)
             {
                 throw new NotSupportedException("The default UI requires a user store with email support.");
             }
-            return (IUserEmailStore<IdentityUser>)_userStore;
+            return (IUserEmailStore<ApplicationUser>)_userStore;
         }
     }
 }
--- TestAuthentification/Controllers/HomeController.cs
+++ TestAuthentification/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TestAuthentification.Data;
+using TestAuthentification.Data.Seed;
 using TestAuthentification.Models;
 
 namespace TestAuthentification.Controllers
@@ -9,14 +12,23 @@ namespace TestAuthentification.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
         {
             _logger = logger;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
         }
 
+        [AllowAnonymous] 
         public IActionResult Index()
         {
+            DatabaseSeeder.Seed(_context, _roleManager, _userManager);
             return View();
         }
 
